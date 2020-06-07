bbf34a6373a8fd7e3a137d9f7e492d058ec693a3
saka-21 <kasaikasai572@gmail.com>
2020-06-07 22:46:50 +0900

    mod: blog/urls.pyの変更

--- blog/urls.py
+++ blog/urls.py
@@ -10,7 +10,7 @@ from .views import (
 
 app_name = 'blog'
 urlpatterns = [
-    path('', IndexView.as_view(), name='post_detail'),
+    path('', IndexView.as_view(), name='index'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
     path('tags/', TagListView.as_view(), name='tag_list'),
